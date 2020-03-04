f3d66bae932e53c78da9970ed2ecada2f8d2ae83
Sreeraj V R <40494878+SVR666@users.noreply.github.com>
2020-03-04 07:44:32 +0530

    sql fix

--- winloss.php
+++ winloss.php
@@ -5,13 +5,13 @@
     include 'config.php';
     if ($_POST['win_loss'] == "loss")
     {
-      $sql = "UPDATE winloss SET loss = loss+1 WHERE username = $uid;";
+      $sql = "UPDATE winloss SET loss = loss+1 WHERE username = '$uid';";
       pg_query($conn, $sql);
       pg_close($conn);
     }
     elseif ($_POST['win_loss'] == "win")
     {
-      $sql = "UPDATE winloss SET win = win+1 WHERE username = $uid;";
+      $sql = "UPDATE winloss SET win = win+1 WHERE username = '$uid';";
       pg_query($conn, $sql);
       pg_close($conn);
     }
