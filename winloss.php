73e239900b3a4980853748314d8b70183cbbf704
svr666 <sreerajvr24@gmail.com>
2020-03-15 13:52:18 +0530

    misc 1

--- winloss.php
+++ winloss.php
@@ -12,7 +12,7 @@
     }
     elseif ($_POST['win_loss'] == "win")
     {
-      $sqlw = "UPDATE winloss SET win = win+1 WHERE username = '$uid';";
+      $sqlw = "UPDATE winloss SET win = win+1, coins =coins+'$coin' WHERE username = '$uid';";
       pg_query($conn, $sqlw);
       pg_close($conn);
     }
